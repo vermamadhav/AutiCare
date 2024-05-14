420724789e3fa0c59fea53e21cf5f7a8b268bc0b
Madhav Verma <madhavverma.hll@gmail.com>
2024-05-14 16:07:06 +0530

    Initial Commit

---
+++ AutiCare/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  AutiCare
+//
+//  Created by Batch-1 on 16/05/24.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
