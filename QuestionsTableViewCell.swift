ad02ceb079db58fd22aba986d50ee223c098dff8
Madhav Verma <madhavverma.hll@gmail.com>
2024-05-17 08:51:02 +0530

    Assesment build

---
+++ QuestionsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionsTableViewCell.swift
+//  AutiCare
+//
+//  Created by Madhav Verma on 16/05/24.
+//
+
+import UIKit
+
+class QuestionsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
